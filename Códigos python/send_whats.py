9bf8ec6303a695bc34db85ec73e79653ab288a42
Jean_Carlos <79288480+jean-script@users.noreply.github.com>
2022-08-10 20:01:35 -0300

    Update send_whats.py

--- Códigos python/send_whats.py
+++ Códigos python/send_whats.py
@@ -1,6 +1,6 @@
 import pywhatkit as kit
 from pywhatkit.remotekit import start_server
 
-kit.sendwhatmsg("967413976", "Te amo", 18,50,30)
+kit.sendwhatmsg("numero", "mensage", 18,50,30)
 
 # pip uninstall pywhatkit # como desistalar biblioteca no python
